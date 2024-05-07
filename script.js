f4ed5e4602f48e5ed2d79ff0f66d2b989370ae8e
fedtoscano <federicotoscano611@gmail.com>
2024-05-07 13:06:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//Chiedi all’utente/essa il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+
+
+
+const userName = prompt("Inserisci il tuo nome")
+const userSurname = prompt("Inserisci il tuo cognome")
+const userColor = prompt("Inserisci il tuo colore preferito")
+
+console.log(`La tua password è: ${userName}${userSurname}${userColor}24`)
